4d33c5f7a5ece686b997618f8f62c17eb6f4c7db
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-06-17 18:56:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,9 @@ def home():
         })
 
     except Exception as e:
-        return jsonify({"hata": str(e)})
+        return jsonify({"hata": str(e)})  
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
