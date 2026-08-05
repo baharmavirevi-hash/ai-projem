225037b2d9e646f9f12b3f93b417bb1e4c71c954
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-05 15:47:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ import os
 
 app = Flask(__name__)
 
-# Upload klasörü
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# Klasör yoksa oluştur
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Route'ları yükle
 init_db()
+
 register_routes(app)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=10000,
+        debug=False
+    )
