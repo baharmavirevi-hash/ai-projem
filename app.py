005ae9e8760d2c84c61260dd7dfb94c59d934c1e
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-06 13:23:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from database import init_db
 from flask import Flask
 from routes import register_routes
+from database import init_db
 import os
 
 app = Flask(__name__)
@@ -8,16 +8,18 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# uploads klasörü yoksa oluştur
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
+# Veritabanını oluştur
 init_db()
 
+# Route'ları yükle
 register_routes(app)
-
 
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=10000,
-        debug=False
+        port=int(os.environ.get("PORT", 5000))
     )
