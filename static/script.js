cb8bbcaacbf920187a80c6e4732f2739541536c1
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-06 23:13:23 +0300

    Implement splash screen hide on load
    Add splash screen functionality to hide after 2 seconds

--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
+window.addEventListener("load", function(){
 
+    setTimeout(function(){
+
+        const splash = document.getElementById("splash-screen");
+
+        if(splash){
+
+            splash.style.opacity = "0";
+
+            setTimeout(function(){
+
+                splash.style.display = "none";
+
+            },600);
+
+        }
+
+    },2000);
+
+});
