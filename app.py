75a10cbecd95cbf0911b57b7998f9267411726cb
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-06 18:48:26 +0300

    Refactor app.py and update debug configuration
    Refactor app.py for improved readability and set debug to False.

--- app.py
+++ app.py
@@ -3,22 +3,30 @@ from routes import register_routes
 from database import init_db
 import os
 
+
 app = Flask(__name__)
+
 
 app.config["UPLOAD_FOLDER"] = "static/uploads"
 
-# Upload klasörü yoksa oluştur
-os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-# Veritabanını oluştur
+os.makedirs(
+    app.config["UPLOAD_FOLDER"],
+    exist_ok=True
+)
+
+
 init_db()
 
-# Route'ları yükle
+
 register_routes(app)
 
+
+
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=True
+        port=int(os.environ.get("PORT",5000)),
+        debug=False
     )
