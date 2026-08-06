55189e88ba3d2ced95d8bb49c28d14e740af1ddc
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-06 13:33:13 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,66 +1,23 @@
 import sqlite3
 
-DB_NAME = "mavigpt.db"
+DB_NAME = "chat.db"
 
+def get_db():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_db()
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS chats(
+    CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        chat_type TEXT,
-        message TEXT,
-        answer TEXT,
-        image TEXT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        user_message TEXT,
+        ai_message TEXT
     )
     """)
-
-    conn.commit()
-    conn.close()
-
-
-def save_chat(chat_type, message, answer, image=None):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    INSERT INTO chats(chat_type, message, answer, image)
-    VALUES (?, ?, ?, ?)
-    """, (chat_type, message, answer, image))
-
-    conn.commit()
-    conn.close()
-
-
-def get_chats(chat_type):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    SELECT id, message, answer, image
-    FROM chats
-    WHERE chat_type=?
-    ORDER BY id ASC
-    """, (chat_type,))
-
-    chats = cursor.fetchall()
-
-    conn.close()
-
-    return chats
-
-
-def delete_chat(chat_id):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "DELETE FROM chats WHERE id=?",
-        (chat_id,)
-    )
 
     conn.commit()
     conn.close()
