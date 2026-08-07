e6078b0ca02d6df1d92fd4bb2519157e6a326a4d
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-07 12:45:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,15 @@ from routes import register_routes
 from database import init_db
 import os
 
-
 app = Flask(__name__)
 
+app.config["UPLOAD_FOLDER"] = os.path.join("static", "uploads")
 
-app.config["UPLOAD_FOLDER"] = "static/uploads"
-
-
-os.makedirs(
-    app.config["UPLOAD_FOLDER"],
-    exist_ok=True
-)
-
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 init_db()
 
-
 register_routes(app)
 
-
-
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT",5000)),
-        debug=False
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
