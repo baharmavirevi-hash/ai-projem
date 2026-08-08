c6e7386ca2678da43d02c8af28cf344fcb6d6de9
baharmavirevi-hash <baharmavirevi@gmail.com>
2026-08-08 17:40:39 +0300

    Format app.py for improved readability

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from routes import register_routes
 from database import init_db
 import os
 
+
 app = Flask(__name__)
 
-app.config["UPLOAD_FOLDER"] = os.path.join("static", "uploads")
+app.config["UPLOAD_FOLDER"] = os.path.join(
+    "static",
+    "uploads"
+)
 
 os.makedirs(
     app.config["UPLOAD_FOLDER"],
